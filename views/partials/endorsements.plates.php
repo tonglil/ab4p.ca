dba82f7665c731d183b115fe85491accdb9c1ed4
Tony Li <tonyli.lives@gmail.com>
2015-03-05 12:18:45 +0800

    wip: endorsements

--- views/partials/endorsements.plates.php
+++ views/partials/endorsements.plates.php
@@ -3,32 +3,61 @@
         <div class="item active">
             <div class="row">
                 <div class="col-lg-4">
-                    <img class="img-circle" src="https://scontent.xx.fbcdn.net/hphotos-xfp1/t31.0-8/10482063_10152893108359400_5782904829331527627_o.jpg" style="width: 140px; height: 140px;">
+                    <img class="img-circle" src="" style="width: 140px; height: 140px;">
                     <h2>
-                        Evan Bauster
+                        Betty Bailey
+                        <br>
+                        <small>
+                            Aaron's Mother
+                        </small>
                     </h2>
                     <p>
-                        Such a great guy.
+                        "If my vote could count, you know where it would be.
+                        I have never known an individual who can set a goal in his mind and work endlessly to see it accomplished.
+                    </p>
+                    <p>
+                        You are a perfect example that hard work and a vision will always pay off in the end.
+                        I am very proud to know you and to watch you reach all of your goals in life."
                     </p>
                 </div>
 
                 <div class="col-lg-4">
-                    <img class="img-circle" src="https://scontent.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/10998408_740768359352037_5330310157651156062_n.jpg?oh=5d39d36ba01f605a4c46a14b53d42c98&oe=55880886" style="width: 140px; height: 140px;">
+                    <img class="img-circle" src="" style="width: 140px; height: 140px;">
                     <h2>
-                        Arial Reen
+                        Visha Thamboo
+                        <br>
+                        <small>
+                            UBC Student Ambassador
+                        </small>
                     </h2>
                     <p>
-                        When will I get my free slice of pizza?
+                        "Aaron Bailey, what a phenomenal human being.
+                        I remember meeting this majestic creature in my second year and being completely and thoroughly inspired.
+                    </p>
+                    <p>
+                        I could not think of a better person to represent the student body of UBC.
+                        He is extremely hard working and abundant in both experiences and resources, which is why I'll be voting for Aaron Bailey to be my 106th AMS president of the University of British Columbia."
                     </p>
                 </div>
 
                 <div class="col-lg-4">
-                    <img class="img-circle" src="https://scontent.xx.fbcdn.net/hphotos-xpf1/v/t1.0-9/10929014_906145336062456_8577813489262620489_n.jpg?oh=51401d4fef407b734a8609bcd5910e21&oe=55830437" style="width: 140px; height: 140px;">
+                    <img class="img-circle" src="" style="width: 140px; height: 140px;">
                     <h2>
-                        Curt Savior
+                        Liam Reeve
+                        <br>
+                        <small>
+                            AMS Councillor - AUS
+                        </small>
                     </h2>
                     <p>
-                        He is soooooooo datable!
+                        "Few people in life can manage to impress me on such a scale as Aaron Bailey has.
+                        I cannot think of any aspect of life on this campus that has not been positively affected from his hard work and perpetual drive.
+                    </p>
+                    <p>
+                        Therefore, it is with great pride that I stand behind him in his efforts to become the next AMS President.
+                    </p>
+                    <p>
+                        As a brother, as a friend, he is without a doubt one of the most amazing people I’ve ever met."
                     </p>
                 </div>
             </div><!-- /.row -->
@@ -37,32 +66,77 @@
         <div class="item">
             <div class="row">
                 <div class="col-lg-4">
-                    <img class="img-circle" src="https://scontent.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/10898022_10152576931686631_826155784875656938_n.jpg?oh=eaf3603df0357fb58d43c1d974218077&oe=5595866C" style="width: 140px; height: 140px;">
+                    <img class="img-circle" src="" style="width: 140px; height: 140px;">
                     <h2>
-                        Diana Ag
+                        Riley Fisher
+                        <br>
+                        <small>
+                            Forestry Undergraduate Society President
+                        </small>
                     </h2>
                     <p>
-                        I walked pass him once!
+                        "I have had the pleasure of knowing Aaron for the last 2 years, and have been on AMS Council for the last year and a half with him.
+                        During this time it has become evident that he is a indisputable asset to the student body.
+                        He has been involved with the on campus community in a multitude of different capacities during his time here at UBC.
+                        He is someone who has made positive impacts on the student body, and wants to continue to better the lives of students.
+                        I believe that with his level of experience, his proven track record, and a clear and realistic platform, Aaron is ready to be the President of the AMS and serve his second term on the UBC Senate
+                    </p>
+                    <p>
+                        I, Riley Fisher, endorse Aaron Bailey for AMS President and UBC Senate."
                     </p>
                 </div>
 
                 <div class="col-lg-4">
-                    <img class="img-circle" src="https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xpf1/v/t1.0-9/1014061_615395808526447_381550776_n.jpg?oh=a52ebb530ba4b5550b0433c1647ad144&oe=55932FCA&__gda__=1435192451_93ac1f0f6e14d09bb65344220adc6d74" style="width: 140px; height: 140px;">
+                    <img class="img-circle" src="" style="width: 140px; height: 140px;">
                     <h2>
-                        Poon Tsunami
+                        Andy Jeun
+                        <br>
+                        <small>
+                            2014/2015 Robson House President
+                        </small>
                     </h2>
                     <p>
-                        God like.
+                        "I’ve known Aaron Bailey for the past three years and he is one of the few leaders on campus who is a constant inspiration to me.
+                        His passion and commitment motivates me to challenge myself to become a more influential leader.
+                        Aaron was one of my Residence Advisors in my first year, and he helped build a community in a way only a few people have in the past.
+                        His initiatives and genuine passion to building community brought a lot of his residents out of their shells.
+                        Through his guidance in my first year, I was able to inspire others the way he inspired me.
+                        He is a great mentor and a good friend.
+                    </p>
+                    <p>
+                        These past three years, I have constantly been amazed by his involvements and achievements.
+                        I know that there is no one who is as qualified, hardworking, and prepared as Aaron Bailey to be your AMS President.
+                        Aaron Bailey will make positive differences and I, Andy Jeun, strongly recommend that you vote Aaron Bailey for 106th AMS President."
                     </p>
                 </div>
 
                 <div class="col-lg-4">
-                    <img class="img-circle" src="https://fbcdn-sphotos-b-a.akamaihd.net/hphotos-ak-xfp1/v/t1.0-9/10945633_10204836174117044_9199181051449687882_n.jpg?oh=902f847cf1df261de6fd2f2180f8cf9c&oe=554A4E8B&__gda__=1435425774_ef8524ba1493fff5611c9f5a749de51f" style="width: 140px; height: 140px;">
+                    <img class="img-circle" src="" style="width: 140px; height: 140px;">
                     <h2>
-                        Alexis Old
+                        Josh Bourke
+                        <br>
+                        <small>
+                            Phi Delta Theta Fraternity President
+                        </small>
                     </h2>
                     <p>
-                        What are you interviewing me for?
+                        "It is with great pride that I stand behind Aaron Bailey in his efforts to be elected the 106th AMS President.
+                    </p>
+                    <p>
+                        Having known Aaron for 2 years, I have been fortunate enough to see the passion he possesses for the betterment of student life for all of us.
+                        It only takes walking around campus for 10 minutes with Aaron to understand just how many people he has personally impacted the life of, and how well connected and respected this man is.
+                    </p>
+                    <p>
+                        UBC is lucky to have Aaron Bailey, and it is with full confidence that I can say he is the ideal candidate to lead our Alma Mater with direction, transparency, confidence and ALWAYS with the improvement of student life being his priority beyond any kind of personal or political agenda.
+                    </p>
+                    <p>
+                        We as UBC students need a better student life, we need to have our university experience be fun instead of being just another number in a profit making machine, we need to have someone in our corner fighting for us tirelessly until what we believe is listened to, and we need a leader who will stand up for what is right with experience, passion, and eloquence.
+                    </p>
+                    <p>
+                        We need Aaron Bailey.
+                    </p>
+                    <p>
+                        It is with this in mind that I urge you to take the opportunity to change this university for the better by voting Aaron Bailey for AMS President."
                     </p>
                 </div>
             </div><!-- /.row -->
