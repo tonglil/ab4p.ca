4bb340b4ac453e42dee5d7dff18b07099abd24b0
Tony Li <tonyli.lives@gmail.com>
2015-03-08 23:21:58 +0800

    hide text on president page

--- views/pages/president.plates.php
+++ views/pages/president.plates.php
@@ -22,36 +22,40 @@ $this->layout('template', [
 
 <ol>
     <li>
-        Community Showcase
-        <ul>
+        <h4 data-toggle="collapse" href="#collapse1">
+            Community Showcase
+        </h4>
+        <ul class="collapse" id="collapse1">
             <li>
                 I pledge to attend at least one event per week organized by a different student organization on campus, and document my experience publicly.
             </li>
             <li>
-                RATIONALE:
+                <span class="strong">RATIONALE</span>:
                 This goal will be a concrete display of the AMS' concern for and interest in the efforts of students to make campus a more engaging and vibrant community.
             </li>
             <li>
-                CONSEQUENCE:
+                <span class="strong">CONSEQUENCE</span>:
                 If a week is skipped where I do not attend an event, I will dedicate either a Saturday or Sunday morning to work in the office, in addition to the regularly expected 5 days.
             </li>
         </ul>
     </li>
 
     <li>
+        <h4 data-toggle="collapse" href="#collapse2">
         Presidential Pitchers
-        <ul>
+        </h4>
+        <ul class="collapse" id="collapse2">
             <li>
                 I will host a weekly office hour on Fridays at the Perch that will be open to all students to directly voice concerns and ideas to me over a rotating pitcher of craft beer (or other non-alcoholic beverage), compliments of yours truly.
                 In addition, I will host a member of the senior UBC Administration at a Presidential Pitchers event once per month, so students can raise their concerns about the university directly to key decision makers.
             </li>
             <li>
-                RATIONALE:
+                <span class="strong">RATIONALE</span>:
                 This goal will open a unique, casual channel of communication between UBC students and the AMS Executive, where they can share concerns, ask questions, or simply interact face to face as peers.
                 This goal will also allow for better connection between students and the University Executive, creating an opportunity for more accountable lobbying by the AMS on student affairs.
             </li>
             <li>
-                CONSEQUENCE:
+                <span class="strong">CONSEQUENCE</span>:
                 If I am unable to attend a Presidential Pitchers event, I will purchase two appetizers at the following week's meet-up to share.
             </li>
         </ul>
@@ -66,26 +70,30 @@ $this->layout('template', [
 
 <ol>
     <li>
+        <h4 data-toggle="collapse" href="#collapse3">
         Bring Back A(ms)CF
-        <ul>
+        </h4>
+        <ul class="collapse" id="collapse3">
             <li>
-                Through large-scale, dedicated, and thoughtful collaboration between the AMS and all interested student organizations on campus, I will spearhead the return of Arts County Fair and the reimagining of the AMS Block Party celebration, rebranded as the AMS County Fair, to be held on the last day of classes at Thunderbird Stadium.
+                Through large-scale, dedicated, and thoughtful collaboration between the AMS and all interested student organizations on campus, I will spearhead the expansion of the AMS Block Party in the likeness of the historic and legendary Arts County Fair.
             </li>
             <li>
-                RATIONALE:
+                <span class="strong">RATIONALE</span>:
                 This goal will cement the AMS' commitment to creating memorable and fun experiences for students as a priority for the Society, and will re-establish UBC as a campus community that is willing to dream big and take meaningful risks together.
             </li>
         </ul>
     </li>
 
     <li>
+        <h4 data-toggle="collapse" href="#collapse4">
         AMS 2030
-        <ul>
+        </h4>
+        <ul class="collapse" id="collapse4">
             <li>
                 Using the foundations laid by this year's AMS Executive, I intend to develop a comprehensive strategic plan for the Society through consultation with Councillors, constituencies, clubs, and students-at-large.
             </li>
             <li>
-                RATIONALE:
+                <span class="strong">RATIONALE</span>:
                 Due to the temporary nature of the AMS Executive, it can be hard to establish and maintain long-term societal goals with a yearly turnover.
                 By creating a guiding document through a by-students-for-students process, the AMS can determine a concrete direction that will provide a point of reference for its future Directors.
             </li>
@@ -93,26 +101,30 @@ $this->layout('template', [
     </li>
 
     <li>
+        <h4 data-toggle="collapse" href="#collapse5">
         Beer That Gives Back
-        <ul>
+        </h4>
+        <ul class="collapse" id="collapse5">
             <li>
                 I will continue to lead the AMS' collaborative efforts with the Faculty of Land and Food Systems to establish an AMS Microbrewery on campus as part of the new UBC Farm Center.
             </li>
             <li>
-                RATIONALE:
+                <span class="strong">RATIONALE</span>:
                 This goal will create more student interest in the south campus area, increase contributions to the Society's endowment through sales at AMS businesses and keg sales for Special Occasion License holders, and provide students with access to inexpensive, high-quality craft beer.
             </li>
         </ul>
     </li>
 
     <li>
+        <h4 data-toggle="collapse" href="#collapse6">
         AMS Whistler Shuttle
-        <ul>
+        </h4>
+        <ul class="collapse" id="collapse6">
             <li>
                 Using proceeds from the sale of the AMS Whistler Lodge, I will work to create a cost-effective shuttle transport solution for AMS members looking to travel from the UBC campus to Whistler on the weekend.
             </li>
             <li>
-                RATIONALE:
+                <span class="strong">RATIONALE</span>:
                 Our proximity to Whistler is a major selling point for students.
                 With such a large population of skiers and snowboarders, the Society should continue to support student interest in winter sports in light of the sale of our AMS Whistler Lodge through a reliable and affordable transportation solution.
             </li>
